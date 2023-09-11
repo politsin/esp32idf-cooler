d4ed4e508e682a5a42072b90d193e5d478ba1e78
Anatoly Politsin <politsin@gmail.com>
2023-09-11 23:58:41 +0300

    refa

--- main/task/fanTask.cpp
+++ main/task/fanTask.cpp
@@ -8,7 +8,6 @@
 
 #define LEDC_TIMER LEDC_TIMER_0
 #define LEDC_MODE LEDC_LOW_SPEED_MODE
-#define LEDC_OUTPUT_IO (5) // Define the output GPIO
 #define LEDC_CHANNEL LEDC_CHANNEL_0
 #define LEDC_DUTY_RES LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
 #define LEDC_DUTY (4095)      // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
@@ -22,23 +21,27 @@ void fanTask(void *pvParam) {
       .duty_resolution = LEDC_DUTY_RES,
       .timer_num = LEDC_TIMER,
       .freq_hz = LEDC_FREQUENCY, // Set output frequency at 5 kHz
-      .clk_cfg = LEDC_AUTO_CLK};
+      .clk_cfg = LEDC_AUTO_CLK,
+  };
   ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
 
   // Prepare and then apply the LEDC PWM channel configuration
-  ledc_channel_config_t ledc_channel = {.gpio_num = LEDC_OUTPUT_IO,
-                                        .speed_mode = LEDC_MODE,
-                                        .channel = LEDC_CHANNEL,
-                                        .intr_type = LEDC_INTR_DISABLE,
-                                        .timer_sel = LEDC_TIMER,
-                                        .duty = 0, // Set duty to 0%
-                                        .hpoint = 0};
-  // esp_rom_gpio_pad_select_gpio(LED_PIN1);
-  // gpio_set_direction(LED_PIN1, GPIO_MODE_OUTPUT);
-  // int isOn = 0;
+  ledc_channel_config_t ledc_channel = {
+      .gpio_num = GPIO_NUM_19,
+      .speed_mode = LEDC_MODE,
+      .channel = LEDC_CHANNEL,
+      .intr_type = LEDC_INTR_DISABLE,
+      .timer_sel = LEDC_TIMER,
+      .duty = 70, // Set duty to 0%
+      .hpoint = 0,
+  };
+  ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
   while (true) {
-    // gpio_set_level(LED_PIN1, isOn);
     vTaskDelay(2000 / portTICK_PERIOD_MS);
+    // Set duty to 50%
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
     if (true) {
       ESP_LOGW(FAN_TAG, "FAN!");
     }
